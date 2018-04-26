bb000a655dcf7613647d3e8bcaa8300e828a3828
lazylock <sendwinston@gmail.com>
2018-04-26 03:08:53 -0700

    button

---
+++ js/app.js
@@ -0,0 +1,81 @@
+var map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            var target = {
+                lat: 37.424315,
+                lng: -122.170150
+            }
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+
+            var distance = measure(pos.lat, pos.lng, target.lat, target.lng);
+            console.log(distance);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+function measure(lat1, lon1, lat2, lon2) {
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d * 1000; // meters
+}
+
+var timer = new Timer();
+timer.start({ countdown: true, startValues: { seconds: 300 } });
+
+$('#countdownExample .values').html(timer.getTimeValues().toString());
+timer.stop();
+
+$('.button').mouseup(function () {
+    timer.reset();
+    timer.stop();
+});
+
+$('.button').mousedown(function () {
+    timer.start();
+});
+
+timer.addEventListener('secondsUpdated', function(e) {
+    $('#countdownExample .values').html(timer.getTimeValues().toString());
+});
+
+timer.addEventListener('targetAchieved', function(e) {
+    $('#countdownExample .values').html('312');
+});
+
